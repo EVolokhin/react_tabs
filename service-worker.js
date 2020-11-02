8ff0024d2ec1c13dd1219da6dfcc24b08f4a1e4f
Eugene <evolokhin@gmail.com>
2020-11-02 15:58:08 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.de263c8b5080c3ff2ed10a87af913dff.js"
 );
 
 self.addEventListener('message', (event) => {
